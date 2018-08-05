176e6dc89738b4e759ab44caadee6f6cf7060d37
Nguyen Mai <nguyenthi.mai@petronas.com.my>
2018-08-05 18:02:07 +0800

    - Added add/edit user viewmodel.

---
+++ A - BACK_END/ApiClientShared/ViewModel/User/EditUserViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace ApiClientShared.ViewModel.User
+{
+    public class EditUserViewModel
+    {
+      
+        public string FirstName { get; set; }
+
+      
+        public string LastName { get; set; }
+     
+       
+        public double? Birthday { get; set; }
+
+     
+    }
+}
