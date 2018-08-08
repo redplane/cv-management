6478e6011ea5ff81f9815a672c35787faf138a72
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-08-08 10:39:22 +0800

    - Add EditUserViewModel properties.

--- A - BACK_END/ApiClientShared/ViewModel/User/EditUserViewModel.cs
+++ A - BACK_END/ApiClientShared/ViewModel/User/EditUserViewModel.cs
@@ -1,16 +1,31 @@
-﻿namespace ApiClientShared.ViewModel.User
+﻿using ApiMultiPartFormData.Models;
+
+namespace ApiClientShared.ViewModel.User
 {
     public class EditUserViewModel
     {
-      
+        #region Properties
+
+        /// <summary>
+        /// First name of user.
+        /// </summary>
         public string FirstName { get; set; }
 
-      
+        /// <summary>
+        /// User last name
+        /// </summary>
         public string LastName { get; set; }
-     
-       
+
+        /// <summary>
+        /// Birthday (UNIX time)
+        /// </summary>
         public double? Birthday { get; set; }
 
-     
+        /// <summary>
+        /// User profile photo.
+        /// </summary>
+        public HttpFile Photo { get; set; }
+
+        #endregion
     }
 }
