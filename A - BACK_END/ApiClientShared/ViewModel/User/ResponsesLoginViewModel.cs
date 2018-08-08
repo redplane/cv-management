a4f31ba737100d96bb8fc0ea7ddc5638a6049be2
Linh Nguyen <redplane_dt@yahoo.com.vn>
2018-08-08 10:54:27 +0800

    - Removed Reading... files. - Changed ResponsesLoginViewModel -> TokenViewModel.

--- A - BACK_END/ApiClientShared/ViewModel/User/ResponsesLoginViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApiClientShared.ViewModel.User
-{
-    public class ResponsesLoginViewModel
-    {
-        public  string AccessToken { get; set; }
-        public  int LifeTime { get; set; }
-        public  string Type { get; set; }
-    }
-}
---
+++ A - BACK_END/ApiClientShared/ViewModel/User/TokenViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ApiClientShared.ViewModel.User
+{
+    public class TokenViewModel
+    {
+        /// <summary>
+        /// Code for accessing system.
+        /// </summary>
+        public  string AccessToken { get; set; }
+
+        /// <summary>
+        /// Token life time (in second)
+        /// </summary>
+        public  int LifeTime { get; set; }
+
+        /// <summary>
+        /// Token type.
+        /// </summary>
+        public  string Type { get; set; }
+    }
+}
